19f78810476be5bd8744b21f8112a54587feb984
SeanHeaslip <sean.j.heaslip@gmail.com>
2017-12-27 16:15:37 +0000

    Fragments added
    Basic fragments added to Assign4. String resource file updated to include page titles.

---
+++ Assign42017SeanHeaslip/app/src/main/java/id_16109759_hdsd/assign42017seanheaslip/FragmentOrder.java
@@ -0,0 +1,22 @@
+package id_16109759_hdsd.assign42017seanheaslip;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by seanh on 27/12/2017.
+ */
+
+public class FragmentOrder extends Fragment
+{
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(R.layout.fragment3_order, container, false);
+
+        return rootView;
+    }
+}
---
+++ Assign42017SeanHeaslip/app/src/main/java/id_16109759_hdsd/assign42017seanheaslip/MyPagerAdapter.java
@@ -0,0 +1,82 @@
+package id_16109759_hdsd.assign42017seanheaslip;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by seanh on 27/12/2017.
+ */
+
+public class MyPagerAdapter extends FragmentPagerAdapter
+{
+//Using Context to call String resource for Page Title
+    Context context;
+    int pageTitleLength = 4;
+
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+
+
+        public MyPagerAdapter(FragmentManager fm)
+        {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position)
+        {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return relevant Fragment (defined as a static inner class below).
+            switch (position)
+            {
+                case 0:
+                    return new FragmentInfo();
+                case 1:
+                    return new FragmentList();
+                case 2:
+                    return new FragmentOrder();
+                case 3:
+                    return new FragmentAccount();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount()
+        {
+            // Show 4 total pages.
+            // note could not figure out how to determine length of resource String-array
+            return pageTitleLength;
+        }
+
+    /**
+     * Citation:
+     * 27/12/2017 @15:20
+     * Referenced use of 'Context' from:
+     * https://stackoverflow.com/questions/18578567/
+     */
+        @Override
+        public CharSequence getPageTitle(int position)
+        {
+            //Removed hardcoded Strings
+            // Could try using Array and use position to return the value
+            switch (position)
+            {
+                case 0:
+                    return context.getString(R.string.infoPageTitle);
+                case 1:
+                    return context.getString(R.string.listPageTitle);
+                case 2:
+                    return context.getString(R.string.orderPageTitle);
+                case 3:
+                    return context.getString(R.string.accountPageTitle);
+            }
+            return null;
+        }
+}
